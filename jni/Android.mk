ac7b899a713d532b5a19747437dfc9b3c4b5055c
Ramdisk01 <bwrp17001499@outlook.com>
2026-07-11 12:52:55 +0800

    Remove: Remove the static linking dependency on the openssl/boringssl
    Because of a license conflict.

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,21 +2,21 @@ LOCAL_PATH := $(call my-dir)
 
 # ---- 根据 ABI 选择对应的库文件 ----
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    CRYPTO_LIB := libcrypto_32.a
+    CRYPTO_LIB := libcrypto_32.so
     KEY_LIB    := subaru_key_armv7a.a
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-    CRYPTO_LIB := libcrypto.a
+    CRYPTO_LIB := libcrypto.so
     KEY_LIB    := subaru_key_aarch64.a
 else
     $(error Unsupported ABI: $(TARGET_ARCH_ABI))
 endif
 
-# ---- libcrypto 预编译库 ----
+# ---- STUB libcrypto ----
 include $(CLEAR_VARS)
-LOCAL_MODULE := libcrypto-prebuilt
-LOCAL_SRC_FILES := mini_openssl_prebuilt/$(CRYPTO_LIB)
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/mini_openssl_prebuilt/include
-include $(PREBUILT_STATIC_LIBRARY)
+LOCAL_MODULE := libcrypto
+LOCAL_SRC_FILES := boringssl/$(CRYPTO_LIB)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/boringssl/include
+include $(PREBUILT_SHARED_LIBRARY)
 
 # ---- private_key_object 预编译库 ----
 include $(CLEAR_VARS)
@@ -28,11 +28,13 @@ include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 LOCAL_MODULE := avb_mini_signer
 LOCAL_SRC_FILES := main.c
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/mini_openssl_prebuilt/include
-LOCAL_STATIC_LIBRARIES := libcrypto-prebuilt private_key_object
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/boringssl/include
+LOCAL_STATIC_LIBRARIES := private_key_object
+
+LOCAL_SHARED_LIBRARIES := libcrypto
 LOCAL_CFLAGS := -O2 -fdata-sections -ffunction-sections -fvisibility=hidden -std=c11 -fPIE -Wall -Wextra
-LOCAL_LDFLAGS := -static \
+LOCAL_LDFLAGS := -pie \
                  -Wl,--build-id=md5 \
                  -Wl,--gc-sections \
-                 -Wl,-Map=output.map
+                 -Wl,-Map=$(LOCAL_PATH)/../obj/local/$(TARGET_ARCH_ABI)/output.map
 include $(BUILD_EXECUTABLE)
